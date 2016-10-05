c9f4c59ef5df5dcf07829d32b6d8eabe061f3b7d
Olga Sizova <olisizova@gmail.com>
2016-10-04 21:05:55 -0400

    created event handler for key down event to move basket

--- scripts/egg_game_pg_2.js
+++ scripts/egg_game_pg_2.js
@@ -1,5 +1,36 @@
+// jQuery for Catch the Eggs Game
+
+
+// Download DOM before running jQuery script
 $(document).ready(onLoad);
 
+// Create global object so jQuery doesn't have to
+// repeat the search for elements in the DOM
+var scope = {};
+
+// begin jQuery execution
 function onLoad(){
 
+  scope.$basket = $('div.basket');
+  scope.$allChickens = $('div.chicken');
+
+// add keydown event listener to move basket
+  $(document).keydown(moveBasket);
+
 }
+ // create event handler for keydown event
+function moveBasket(event){
+  // check for right and left arrow keys
+  // if right key then move basket to the right
+  if (event.key == 'ArrowRight'){
+    scope.$basket.css('left', '+=15');
+// if left key then move basket to the left
+  } else if (event.key == 'ArrowLeft'){
+    scope.$basket.css('left', '-=15');
+
+
+
+
+  }
+
+};
