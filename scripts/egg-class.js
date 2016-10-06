d2f8d5c7ab5d06b5f2ed386e6819d52f9d60fc3a
Olga Sizova <olisizova@gmail.com>
2016-10-05 21:41:08 -0400

    test to create new egg divs in js

--- scripts/egg-class.js
+++ scripts/egg-class.js
@@ -1,3 +1,24 @@
+// Download DOM before running jQuery script
+$(document).ready(onLoad);
+
+
+function onLoad(){
+
+    var firstChickenDiv = $('div.chicken').first();
+
+    var startHatchParam = {
+      left: 100,
+      top: 700,
+      $chickenDiv: firstChickenDiv
+    }
+
+    // This is a test function to work with the egg
+    var testEgg = new Egg(startHatchParam);
+    console.log(testEgg.startPosLeft);
+}
+
+
+
 // Create Egg Class in a separate js files as a prototype
 // for mulitple eggs
 
@@ -8,6 +29,7 @@ function Egg(startHatch){
   // of future egg objects
   this.startPosLeft = startHatch.left;
   this.starPosTop = startHatch.top;
+  this.$chickenDiv = startHatch.$chickenDiv;
 
   // create div in memory to hold egg image
   // set width, height and background via class egg in egg_style.css
@@ -15,13 +37,16 @@ function Egg(startHatch){
   this.$eggImageDiv = $('<div></div>').attr('class', 'egg')
                                       .css('left', this.startPosLeft)
                                       .css('top', this.starPosTop);
+  this.$chickenDiv.append(this.$eggImageDiv);
+
+
 
 
 };
 
-// This is a test function to work with the egg
-var testEgg = new Egg({left: 10, top: -5});
-console.log(testEgg.startPosLeft);
+
+
+
 
 
 
